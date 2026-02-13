64f480c5269ae37b8139ba3890e76eceb889fe39
ASWIN V K <vkaswinkanan@gmail.com>
2026-02-13 10:19:05 +0530

    update: change backend endpoint for production

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:5000',
+                target: 'https://digital-issue-tracking-dashboard.onrender.com',
                 changeOrigin: true,
                 secure: false,
             }
